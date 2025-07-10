5fd718cae0f00d5e176ec5cf055574928410edfb
zed <zedekjacob@gmail.com>
2025-07-10 17:37:29 +0100

    basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+add = number1 + number2
+sub = number1 - number2
+mult = number1 * number2
+
+print("Addition of ",number1 ,"and", number2, " is ", add )
+print("Subraction of ",number1 ,"and" ,number2, " is ", sub )
+print("Multiplication of ",number1 ,"and", number2, " is ", mult )
