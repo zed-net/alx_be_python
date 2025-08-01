0d5c4f2a80e88a0f34a6b910856455613ea9e2df
zed <zedekjacob@gmail.com>
2025-08-01 14:22:48 +0100

    division 2

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -6,7 +6,7 @@ def safe_divide(numerator, denominator):
         if den == 0:
             return "Error: Cannot divide by zero."
         result = num / den
-        return result
+        return f"The result of the division is {result}"
 
     except ValueError:
         return "Error: Please enter numeric values only."
