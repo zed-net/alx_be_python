9caffba4c3e25015544ebb9b5c84b96847f9cbb7
zed <zedekjacob@gmail.com>
2025-08-07 20:57:59 +0100

    lib system 2.1

--- oop/library_system.py
+++ oop/library_system.py
@@ -7,7 +7,7 @@ class Book:
 
 class EBook (Book):
     def __init__(self,title,author, file_size):
-        super().__init__(title,author)
+        super().__init__(title,author)  
         self.file_size = file_size
 
     def __str__(self):
