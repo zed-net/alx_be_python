650156b8b97e3d28567170df38d31ec35c6ea606
zed <zedekjacob@gmail.com>
2025-08-07 20:56:34 +0100

    lib system 2

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,7 +3,7 @@ class Book:
         self.title = title
         self.author = author
     def __str__(self):
-        return f"Book: {self.title} by {self.author}"
+        return f"Book: {self.title} by {self.author}"  
 
 class EBook (Book):
     def __init__(self,title,author, file_size):
